9813249e11cbd25e25119be04c410c66b82d51b2
Greg <gregory.bowen@student.uts.edu.au>
2023-09-12 20:59:10 +1000

    initial commit

--- src/gui/graphedge.cpp
+++ src/gui/graphedge.cpp
@@ -5,15 +5,17 @@
 #include "graphedge.hpp"
 #include "graphvertex.hpp"
 
+
 // public:
-GraphEdge::GraphEdge(GraphVertex * p1v, GraphVertex * p2v
+GraphEdge::GraphEdge(GraphVertex * p1v, GraphVertex * p2v, QMenu * menu
                      , QGraphicsItem *parent)
    : QGraphicsLineItem(parent), p1vertex(p1v), p2vertex(p2v)
+   , contextmenu_e(menu)
 {
    // standard edge: colour, width
    setPen(edgecolour);
 
-   // instantiate the edge; position it behind its p1 and p2 vertices
+   // instantiate the edge; position edge behind its p1 and p2 vertices
    QLineF edge(mapFromItem(p1vertex, 10, 10)
       , mapFromItem(p2vertex, 10, 10));
    setLine(edge);
@@ -30,3 +32,6 @@ void GraphEdge::resetEdgePosition() {
       setLine(edge);
    }
 }
+
+
+// protected:
--- src/gui/graphedge.hpp
+++ src/gui/graphedge.hpp
@@ -8,17 +8,25 @@ class GraphVertex;
 
 class GraphEdge : public QGraphicsLineItem
 {
+   QMenu * contextmenu_e {};
+
    QPen edgecolour {Qt::black, 2};
+
    GraphVertex * p1vertex {};
    GraphVertex * p2vertex {};
 
 public:
-   GraphEdge(GraphVertex *, GraphVertex *, QGraphicsItem * parent= nullptr);
+   // qgraphicsitem_cast (type), QGraphicsLineItem::Type = 6
+   GraphEdge(GraphVertex *, GraphVertex *, QMenu *
+             , QGraphicsItem * parent= nullptr);
 
    GraphVertex * p1v() const { return p1vertex; };
    GraphVertex * p2v() const  { return p2vertex; };
 
    void resetEdgePosition();
+
+protected:
+
 };
 
 #endif //TUQ_GRAPHEDGE_HPP
