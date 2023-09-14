ade683f36df3f7c530ea5e6e70b8e99c79a22dc5
Greg <gregory.bowen@student.uts.edu.au>
2023-09-14 15:12:50 +1000

    test: GraphView setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
-#include "gui/mainwindow.hpp"
+#include "gui/graphview.hpp"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
    QApplication tuq(argc, argv);
-   MainWindow w;
-   w.show();
+
+   // move and reformat as helper function: begin
+   auto * view= new GraphView();
+   view->setWindowTitle("tuQ");
+   view->setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
+   // helper function: end
+
+//   view->set_lattice(15,15);
+   view->show();
 
    return tuq.exec();
 }
