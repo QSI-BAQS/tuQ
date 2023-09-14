89bf09d82166159f8312a399c6f7f8ca049386b3
Greg <gregory.bowen@student.uts.edu.au>
2023-09-14 15:14:19 +1000

    test: mousePressEvent, clabel 'E' and 'V'

--- src/gui/graphview.cpp
+++ src/gui/graphview.cpp
@@ -3,18 +3,22 @@
 //
 # include "graphview.hpp"
 
+#include <QKeyEvent>
 
+
+// public:
 GraphView::GraphView(QWidget *parent)
    : QGraphicsView(parent), scene(new QGraphicsScene(this))
 {
+   scene->setSceneRect(-1500,-1500,3000,3000);
+
    setScene(scene);
+   setMouseTracking(true);
 
    clabel= new QLabel;
    clabel->setWindowFlag(Qt::ToolTip);
-   clabel->setAttribute(Qt::WA_TranslucentBackground);
-   clabel->setStyleSheet("background-color: rgba(255, 255, 255, 0);");
+   clabel->setWindowOpacity(0);
 }
-
 
 void GraphView::set_lattice(unsigned long m, unsigned long n) {
 //   circuit-to-graph layout
@@ -29,7 +33,7 @@ void GraphView::set_lattice(unsigned long m, unsigned long n) {
 
    for (int i= 0; i < m; ++i) {
       for (int j= 0; j < n; ++j) {
-         auto vertex= new GraphVertex(id);
+         auto vertex= new GraphVertex(nullptr,id);   // TO DO: initialise, contextmenu
          vertex->setPos(j*xinc,i*yinc);
          scene->addItem(vertex);
 
@@ -38,3 +42,139 @@ void GraphView::set_lattice(unsigned long m, unsigned long n) {
    }
 }
 
+
+// protected:
+void GraphView::keyPressEvent(QKeyEvent * event) {
+   if (event->key() == Qt::Key_E
+   || event->key() == Qt::Key_O
+   || event->key() == Qt::Key_V
+   || event->key() == Qt::Key_X
+   || event->key() == Qt::Key_Y
+   || event->key() == Qt::Key_Z){
+      setCursorLabel(event->text());
+      cursorState= true;
+/*  Q2Graph code includes this + comment for Qt::Key_Z
+      clearSelection()
+*/
+      QGraphicsView::keyPressEvent(event);
+   }
+}
+
+void GraphView::mouseMoveEvent(QMouseEvent * event) {
+   // position the label at x,y relative to cursor
+   if (cursorState){
+      auto qmex {(int) event->screenPos().x()};
+      auto qmey{(int) event->screenPos().y()};
+
+      clabel->move(qmex + 15, qmey + 10);
+   }
+
+   // draw the proxy (Graph)Edge instantiated through mousePressEvent
+   if (tracer != nullptr && clabel->text() == "E"){
+      QLineF growline(tracer->line().p1(), event->screenPos());
+      tracer->setLine(growline);
+   }
+   QGraphicsView::mouseMoveEvent(event);
+}
+
+void GraphView::mousePressEvent(QMouseEvent * event) {
+   // LEFT click events
+   if (event->button() != Qt::LeftButton)
+      return ;
+   // TO DO, LPM X abort insert
+
+   // instantiate: GraphEdge, pt. 1
+   if (clabel->text() == "E"){
+      // collect the p1 vertex at the cursor hotspot at 'click'
+      QPoint vpos= event->pos();
+      QGraphicsItem * edge_origin= itemAt(vpos);
+
+      // prevent runtime exception of no vertex at the cursor hotspot at
+      // 'click'
+      if (edge_origin == nullptr){
+         cursorState= false;
+         clabel->clear();
+         return ;
+      }
+      else
+         edge_origin->setFlag(QGraphicsItem::ItemIsMovable, false);
+
+      // initialise a 'tracer' line segment.  The actual edge will be set in
+      // arrears at the subsequent mouseReleaseEvent
+      tracer= new QGraphicsLineItem(QLineF(vpos,vpos));
+      tracer->setPen(QPen(Qt::black,2));
+
+      scene->addItem(tracer);
+
+      // reset cursor state
+      cursorState= false;
+      clabel->hide();
+   }
+   // local complementation
+   else if (clabel->text() == "O"){
+      // reset cursor state
+      cursorState= false;
+      clabel->clear();
+   }
+   // instantiate: GraphVertex
+   else if (clabel->text() == "V"){
+      // instantiate the vertex
+      auto * v= new GraphVertex(nullptr, vertices_count);   // TO DO: initialise, contextmenu
+      vertices_count += 1;
+
+      // place vertex at 'click' position
+      QPointF pos_xy= mapToScene(event->x()-15, event->y());
+      v->setPos(pos_xy);
+
+      scene->addItem(v);
+
+      // reset cursor state
+      cursorState= false;
+      clabel->clear();
+   }
+   // measurement, x-basis
+   else if (clabel->text() == "X"){
+      // reset cursor state
+      cursorState= false;
+      clabel->clear();
+   }
+   // measurement, y-basis
+   else if (clabel->text() == "Y"){
+      // reset cursor state
+      cursorState= false;
+      clabel->clear();
+   }
+   // measurement, z-basis
+   else if (clabel->text() == "Z"){
+      // reset cursor state
+      cursorState= false;
+      clabel->clear();
+   }
+
+   QGraphicsView::mousePressEvent(event);
+}
+
+void GraphView::mouseReleaseEvent(QMouseEvent * event) {
+   // instantiate: GraphEdge, pt. 2
+
+}
+
+
+// private:
+void GraphView::setCursorLabel(QString tag) {
+   // create E/O/V/X/Y/Z label for cursor
+   // pre-condition: cursorState == true
+   // post-condition: a visible, persistent label of the cursor
+
+   if (cursorState){
+      // label appears at (x,y), relative to cursor
+      clabel->move(QCursor::pos().x() + 15, QCursor::pos().y() + 10);
+
+      // set label
+      tag.isLower() ? clabel->setText(tag.toUpper()) : clabel->setText(tag);
+
+      // reveal a hidden cursor
+      if (clabel->isHidden())
+         clabel->show();
+   }
+}
--- src/gui/graphview.hpp
+++ src/gui/graphview.hpp
@@ -1,32 +1,38 @@
 #ifndef GRAPHVIEW_HPP
 #define GRAPHVIEW_HPP
-#pragma once
 
+#include "graphedge.hpp"
 #include "graphvertex.hpp"
 
 #include <QGraphicsView>
 #include <QLabel>
 
 
+class QGraphicsLineItem;
+
 class GraphView : public QGraphicsView
 {
    Q_OBJECT
 
-// 'class members are private by default'
    QLabel * clabel {};
+   bool cursorState {false};
+   unsigned long edges_count {1};
    QGraphicsScene * scene;
+   QGraphicsLineItem * tracer {};
+   unsigned long vertices_count {1};
 
+   void setCursorLabel(QString);
 
-// 'keep interfaces minimal'
 public:
    explicit GraphView(QWidget *parent= nullptr);
 
    void set_lattice(unsigned long, unsigned long);
 
-
-// 'keep interfaces minimal'
 protected:
-
+   void keyPressEvent(QKeyEvent *) override;
+   void mouseMoveEvent(QMouseEvent *) override;
+   void mousePressEvent(QMouseEvent *) override;
+   void mouseReleaseEvent(QMouseEvent *) override;
 
 };
 
