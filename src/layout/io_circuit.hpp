4f5f9a6ec2b87dc3e303f68473d87f979c07fbb7
Greg <gregory.bowen@student.uts.edu.au>
2023-09-20 11:28:54 +1000

    initial commit

---
+++ src/layout/io_circuit.hpp
@@ -0,0 +1,17 @@
+//
+// input JSON-format
+//
+
+#ifndef ETCH_2022_1_IO_CIRCUIT_HPP
+#define ETCH_2022_1_IO_CIRCUIT_HPP
+
+#include "json.hpp"
+
+#include <iostream>
+#include <vector>
+
+
+// function prototype
+nlohmann::json cirq_to_ionq_schema(const nlohmann::json &);
+
+#endif //ETCH_2022_1_IO_CIRCUIT_HPP
