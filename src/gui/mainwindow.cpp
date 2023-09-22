8ed90ebd4ec461088ae706532f757da5533e304e
Greg <gregory.bowen@student.uts.edu.au>
2023-09-22 18:16:25 +1000

    saveGraphDialog() member; expand createMenus()

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -20,10 +20,17 @@ MainWindow::MainWindow(QWidget *parent)
 //   view->set_lattice(15,15);
 }
 
+void MainWindow::saveGraphDialog() {
+   QString graphsavefile= QFileDialog::getSaveFileName(this, tr("Save File"));
+   view->saveGraph(graphsavefile);
+}
 
 // private:
 void MainWindow::createMenus() {
    fileMenu= menuBar()->addMenu(tr("&File"));
+   fileMenu->addAction(tr("&Save"), this, [this](){
+      saveGraphDialog();
+   });
    fileMenu->addAction(tr("E&xit"), this, [this](){
       QWidget::close();
    });
