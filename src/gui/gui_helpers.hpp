57c00897eab8777ece92dbad3a3a664f0a0e9f36
Greg <gregory.bowen@student.uts.edu.au>
2023-10-12 13:29:37 +1100

    add h_localComplementation

--- src/gui/gui_helpers.hpp
+++ src/gui/gui_helpers.hpp
@@ -1,11 +1,13 @@
 #ifndef TUQ_GUI_HELPERS_HPP
 #define TUQ_GUI_HELPERS_HPP
 
+#include "graphvertex.hpp"
+
 #include <QGraphicsItem>
 #include <QGraphicsScene>
 
 
-unsigned long h_items_count(int, QGraphicsScene *);
-
+unsigned long h_item_counter(int, QGraphicsScene *);
+void h_localComplementation(GraphVertex *);
 
 #endif //TUQ_GUI_HELPERS_HPP
