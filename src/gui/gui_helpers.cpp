b44608457ee8c8ba39e2c89359124c31dc8f1b59
Greg <gregory.bowen@student.uts.edu.au>
2023-10-12 13:30:06 +1100

    update h_item_counter; testing h_localComplementation

--- src/gui/gui_helpers.cpp
+++ src/gui/gui_helpers.cpp
@@ -5,8 +5,11 @@
 #include "gui_helpers.hpp"
 
 
-// count of items in scene by QGraphicsItem::Type
-unsigned long h_items_count(int item_type, QGraphicsScene * scene) {
+unsigned long h_item_counter(int item_type, QGraphicsScene * scene) {
+   // count of items in scene by QGraphicsItem::Type
+   // pre-condition: GraphVertex added to scene by GraphView::mousePressEvent
+   // post-condition: counter assigned to vertexid of latest GraphVertex
+
    unsigned long counter {1};
    for (QGraphicsItem * item : scene->items()) {
       if (item->type() == item_type)
@@ -14,4 +17,19 @@ unsigned long h_items_count(int item_type, QGraphicsScene * scene) {
    }
 
    return counter;
+}
+
+
+void h_localComplementation(GraphVertex * lcv) {
+   // local complementation (LC) applied to in-focus vertex j: all vertices of
+   // j's neighbourhood not joined by a (graph)edge to gain an edge; all
+   // vertices of j's neighbourhood joined by a (graph)edge to lose an edge.
+   // pre-condition: target object is type, GraphVertex
+   // post-condition: edges are reconfigured as consistent with LC
+
+   // LC operation: vertex j has <= 1 edge
+   if (lcv->alledges->count() < 2)
+      return ;
+   // LC operation: vertex j has > 1 edge
+   else {}
 }
