d6a2c500ec3fe75ad6d85902528205162e78111e
Greg <gregory.bowen@student.uts.edu.au>
2023-10-26 16:34:42 +1100

    WiP h_localComplementation

--- src/gui/gui_helpers.cpp
+++ src/gui/gui_helpers.cpp
@@ -1,17 +1,34 @@
 //
 // assorted helper functions
 //
-
 #include "gui_helpers.hpp"
 
+#include <QDialogButtonBox>
+#include <QLabel>
+#include <QDialog>
 
-unsigned long h_item_counter(int item_type, QGraphicsScene * scene) {
+// public:
+InputDialog::InputDialog(QString menu_function, QWidget * parent)
+   : QDialog(parent), dialog_type(menu_function), form(new QFormLayout(this))
+{
+   form->addRow(new QLabel(dialog_type));
+
+   auto * buttonBox= new QDialogButtonBox(QDialogButtonBox::Ok
+         | QDialogButtonBox::Cancel,Qt::Horizontal, this);
+   form->addRow(buttonBox);
+
+   QObject::connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
+   QObject::connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+}
+
+
+unsigned long h_item_counter(int item_type, const QGraphicsScene & scene) {
    // count of items in scene by QGraphicsItem::Type
    // pre-condition: GraphVertex added to scene by GraphView::mousePressEvent
    // post-condition: counter assigned to vertexid of latest GraphVertex
 
    unsigned long counter {1};
-   for (QGraphicsItem * item : scene->items()) {
+   for (QGraphicsItem * item : scene.items()) {
       if (item->type() == item_type)
          counter += 1;
    }
@@ -19,17 +36,27 @@ unsigned long h_item_counter(int item_type, QGraphicsScene * scene) {
    return counter;
 }
 
-
-void h_localComplementation(GraphVertex * lcv) {
-   // local complementation (LC) applied to in-focus vertex j: all vertices of
-   // j's neighbourhood not joined by a (graph)edge to gain an edge; all
-   // vertices of j's neighbourhood joined by a (graph)edge to lose an edge.
+void h_localComplementation(GraphVertex & lcv, QGraphicsScene & scene) {
+   // local complementation (LC) applied to in-focus vertex, lcv: all vertices
+   // of lcv's neighbourhood not joined by a (graph)edge to be joined by an
+   // edge; all vertices of lcv's neighbourhood joined by a (graph)edge to lose
+   // that edge.
    // pre-condition: target object is type, GraphVertex
    // post-condition: edges are reconfigured as consistent with LC
 
-   // LC operation: vertex j has <= 1 edge
-   if (lcv->alledges->count() < 2)
+   // ABORT LC operation: vertex lcv has <= 1 edge
+   if (lcv.alledges->count() < 2)
       return ;
-   // LC operation: vertex j has > 1 edge
-   else {}
+
+   // populate vector of lcv neighbours
+   QVector<GraphVertex *> lcNeighbours;
+   for (const GraphEdge * e : *lcv.alledges) {
+         if (e->p1vertex->pos() == lcv.pos())
+            lcNeighbours.push_back(e->p2vertex);
+         else
+            lcNeighbours.push_back(e->p1vertex);
+   }
+
+   // 1. ADD edges {scene->addItem(e);}; 2. DELETE edges
+   // *** need scene for addItem() ***
 }
--- src/gui/gui_helpers.hpp
+++ src/gui/gui_helpers.hpp
@@ -1,13 +1,29 @@
 #ifndef TUQ_GUI_HELPERS_HPP
 #define TUQ_GUI_HELPERS_HPP
 
+#include "graphedge.hpp"
 #include "graphvertex.hpp"
 
+#include <QDialog>
+#include <QFormLayout>
 #include <QGraphicsItem>
 #include <QGraphicsScene>
+#include <QLineEdit>
 
 
-unsigned long h_item_counter(int, QGraphicsScene *);
-void h_localComplementation(GraphVertex *);
+class InputDialog : public QDialog
+{
+   QString dialog_type;
+
+public:
+   explicit InputDialog(QString, QWidget * parent= nullptr);
+
+   QFormLayout * form;
+
+};
+
+
+unsigned long h_item_counter(int, const QGraphicsScene &);
+void h_localComplementation(GraphVertex &, QGraphicsScene &);
 
 #endif //TUQ_GUI_HELPERS_HPP
