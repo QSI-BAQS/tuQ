41c1a93b0395cac30a457c4a610dbdcbe0d79163
Greg <gregory.bowen@student.uts.edu.au>
2024-01-17 15:22:48 +1100

    initial commit; add InputDialog

--- src/gui/io_dialog.cpp
+++ src/gui/io_dialog.cpp
@@ -1,5 +1,26 @@
 //
-// Created by gmbowen on 1/17/24.
+// input via gui dialog
 //
 
 #include "io_dialog.hpp"
+
+#include <QDialog>
+#include <QDialogButtonBox>
+#include <QLabel>
+#include <QRegularExpression>
+
+
+// public:
+InputDialog::InputDialog(QString menu_function, QWidget * parent)
+      : QDialog(parent), dialog_type(menu_function), form(new QFormLayout(this))
+{
+   form->addRow(new QLabel(dialog_type));
+
+   auto * buttonBox= new QDialogButtonBox(QDialogButtonBox::Ok
+                                          | QDialogButtonBox::Cancel,Qt::Horizontal, this);
+   form->addRow(buttonBox);
+
+   QObject::connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
+   QObject::connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+}
+
--- src/gui/io_dialog.hpp
+++ src/gui/io_dialog.hpp
@@ -1,8 +1,22 @@
-//
-// Created by gmbowen on 1/17/24.
-//
-
 #ifndef TUQ_IO_DIALOG_HPP
 #define TUQ_IO_DIALOG_HPP
+
+#include <QDialog>
+#include <QFormLayout>
+
+
+
+class InputDialog : public QDialog
+{
+   QString dialog_type;
+
+public:
+   explicit InputDialog(QString, QWidget * parent= nullptr);
+
+   QFormLayout * form;
+
+};
+
+
 
 #endif //TUQ_IO_DIALOG_HPP
