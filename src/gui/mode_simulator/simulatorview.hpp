5654a63bb5a4ac86aa3b4d2b8149c23565b9c992
Greg <gregory.bowen@student.uts.edu.au>
2024-06-14 17:06:22 +1000

    initial commit

--- src/gui/mode_simulator/simulatorview.hpp
+++ src/gui/mode_simulator/simulatorview.hpp
@@ -1,13 +1,15 @@
 #ifndef TUQ_SIMULATORVIEW_HPP
 #define TUQ_SIMULATORVIEW_HPP
 
+#include "operatorpalette.hpp"
+
 #include <fstream>
 #include <QGraphicsView>
 
 
 class SimulatorView : public QGraphicsView
 {
-Q_OBJECT
+   Q_OBJECT
 
    QGraphicsScene * s_scene;
 
@@ -16,7 +18,6 @@ public:
 
    void clear_scene() { s_scene->clear(); }
 //   void readCircuit(const QString &);
-
 };
 
 #endif //TUQ_SIMULATORVIEW_HPP
