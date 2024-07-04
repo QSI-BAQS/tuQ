021b0f0e3b8f18d90d04673dfcb6d09542b162d3
Greg <gregory.bowen@student.uts.edu.au>
2024-07-04 16:38:38 +1000

    initial commit

--- src/gui/mode_simulator/signmeasure.cpp
+++ src/gui/mode_simulator/signmeasure.cpp
@@ -1,5 +1,31 @@
 //
-// Created by gmbowen on 7/4/24.
+// represent individual measurement of x/y/z basis
 //
 
 #include "signmeasure.hpp"
+
+
+// public:
+SignMeasure::SignMeasure(QString & sign_measure, QGraphicsItem *parent)
+   : basis(sign_measure) {}
+
+QRectF SignMeasure::boundingRect() const {
+   return {-22,-15,56,56};
+}
+
+void SignMeasure::paint(QPainter * painter
+      , const QStyleOptionGraphicsItem * option
+      , QWidget * widget) {
+   Q_UNUSED(option)
+   Q_UNUSED(widget)
+
+   // measurement tile
+   painter->setPen(QPen(Qt::black,2));
+   painter->setBrush(Qt::white);
+   painter->drawEllipse(boundingRect());
+   // operator key
+   painter->setPen(QPen(Qt::blue,18));
+   painter->setFont(QFont("Times New Roman",24));
+   painter->drawText(boundingRect(),Qt::AlignCenter,basis);
+   painter->setRenderHint(QPainter::Antialiasing);
+}
--- src/gui/mode_simulator/signmeasure.hpp
+++ src/gui/mode_simulator/signmeasure.hpp
@@ -1,8 +1,21 @@
-//
-// Created by gmbowen on 7/4/24.
-//
-
 #ifndef TUQ_SIGNMEASURE_HPP
 #define TUQ_SIGNMEASURE_HPP
+
+#include <QGraphicsSimpleTextItem>
+#include <QPainter>
+
+
+class SignMeasure : public QGraphicsSimpleTextItem
+{
+   QString basis {};
+
+public:
+   // QGraphicsSimpleTextItem::Type = 9
+   explicit SignMeasure(QString &, QGraphicsItem * parent= nullptr);
+
+   QRectF boundingRect() const override;
+   void paint(QPainter *, const QStyleOptionGraphicsItem *
+              , QWidget *) override;
+};
 
 #endif //TUQ_SIGNMEASURE_HPP
