304dd2e38a22f5e3c5ddfd750313ee81913e5912
Greg <gregory.bowen@student.uts.edu.au>
2024-08-05 17:45:00 +1000

    reformat 'boundingRect'

--- src/gui/mode_simulator/signmeasure.cpp
+++ src/gui/mode_simulator/signmeasure.cpp
@@ -1,16 +1,22 @@
 //
-// represent individual measurement of x/y/z basis
+// represent measurements of x/y/z basis
 //
 
 #include "signmeasure.hpp"
 
 
 // public:
-SignMeasure::SignMeasure(QString & sign_measure, QGraphicsItem *parent)
-   : basis(sign_measure) {}
+SignMeasure::SignMeasure(QString & sign_measure, QGraphicsItem * parent)
+   : basis(sign_measure) {/*
+   // properties of the operator
+   setFlag(QGraphicsItem::ItemIsFocusable);
+   setFlag(QGraphicsItem::ItemIsMovable);
+   setFlag(QGraphicsItem::ItemIsSelectable);
+   setFlag(QGraphicsItem::ItemSendsScenePositionChanges);*/
+}
 
 QRectF SignMeasure::boundingRect() const {
-   return {-22,-15,56,56};
+   return {-22,-15,152,56};
 }
 
 void SignMeasure::paint(QPainter * painter
