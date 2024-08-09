accebb5ecff5dfc3f1887817452500c8bed5ee4c
Greg <gregory.bowen@student.uts.edu.au>
2024-08-09 16:43:42 +1000

    replace 'column' pointers with array, columnAtRow; restore function, addRow as slot

--- src/gui/mode_simulator/algorithmlattice.hpp
+++ src/gui/mode_simulator/algorithmlattice.hpp
@@ -12,21 +12,29 @@
 
 class AlgorithmLattice : public QGraphicsScene
 {
+   // 'The Q_OBJECT macro must appear in the private section of a class 
+   // definition that declares its own signals and slots...'
+   Q_OBJECT
+
    // 21 * 21 tiles
    const QRectF latticeDims {-750,-500,4150,1580};
-// TO DO: array of row/column index to cap adds of tiles at 21 * 21?
+
+   // to cap adds of tiles at 21 * 21, use this?
+   unsigned int columnAtRow [21]= {0};
+
    unsigned int maxRow {0};
-   unsigned int nodeColumn {0};
-   unsigned int nodeRow {0};
-   unsigned int * columnMarker= & nodeColumn;
    unsigned int * maxRowMarker= & maxRow;
+
+   unsigned int nodeRow {0};
    unsigned int * rowMarker= & nodeRow;
 
    OperatorPalette * p_operators;
    SignMeasure * p_operatorType;
 
-   void addRow(unsigned int);
-   void placeOperator(QString, unsigned int *);
+   void placeOperator(QString, unsigned int);
+
+private slots:
+   void addRow();
 
 public:
    explicit AlgorithmLattice(QWidget * parent= nullptr);
