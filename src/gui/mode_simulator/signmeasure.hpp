efc521eac23596c292c75ba1f0bea2b284c860a6
Greg <gregory.bowen@student.uts.edu.au>
2024-08-22 14:56:54 +1000

    refactor as QGraphicsItem; add const data, designatePen; patternRect; add datum, p_CZ for linked list

--- src/gui/mode_simulator/signmeasure.hpp
+++ src/gui/mode_simulator/signmeasure.hpp
@@ -9,17 +9,27 @@
 #pragma once
 
 
-class SignMeasure : public QGraphicsSimpleTextItem
+class SignMeasure : public QGraphicsItem
 {
    QString designate {};
 
-public:
-   // QGraphicsSimpleTextItem::Type = 9
-   explicit SignMeasure(QString &, QGraphicsItem * parent= nullptr);
+   const QPen designatePen {Qt::blue, 18};
+   const QRectF patternRect {-22,-15,152,56};
 
-   QRectF boundingRect() const override;
+public:
+   explicit SignMeasure(QString &);
+
+   // SignMeasure::Type = 65537
+   enum { Type= UserType + 1 };
+
+   // proxy for CZ gate on adjacent operators
+   SignMeasure * p_CZ= nullptr;
+
+   QRectF boundingRect() const override { return patternRect; };
+   QString showOperator() { return designate; };
    void paint(QPainter *, const QStyleOptionGraphicsItem *
               , QWidget *) override;
+   int type() const override { return Type; };
 };
 
 #endif //TUQ_SIGNMEASURE_HPP
