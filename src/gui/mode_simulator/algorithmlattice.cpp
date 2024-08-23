f1bf44a83c5c5e1d47a855ea3f9326ef6b5a8279
Greg <gregory.bowen@student.uts.edu.au>
2024-08-23 14:44:22 +1000

    update constructor with p_initialise00; consolidate row operations as method, addRow

--- src/gui/mode_simulator/algorithmlattice.cpp
+++ src/gui/mode_simulator/algorithmlattice.cpp
@@ -14,9 +14,9 @@ AlgorithmLattice::AlgorithmLattice(QWidget * parent)
    p_operatorType= nullptr;
    p_operators= new OperatorPalette();
 
-   p_initialiseRow= new SignMeasure(ket0);
-   p_initialiseRow->setPos(nodeAddress[0][0]);
-   addItem(p_initialiseRow);
+   p_initialise00= new SignMeasure(ket0);
+   p_initialise00->setPos(nodeAddress[0][0]);
+   addItem(p_initialise00);
 
    columnAtRow[*rowMarker] += 1;
    lastInsert= nodeAddress[0][0];
@@ -31,8 +31,7 @@ AlgorithmLattice::AlgorithmLattice(QWidget * parent)
            , columnAtRow[*rowMarker]); });
    // 'add row' button
    connect(p_operators->p_addRow, &QPushButton::clicked, [this](){
-      prepareRow();
-      columnAtRow[*rowMarker] += 1; });
+      addRow(); });
    // 'switch rows' form
    connect(p_operators->possibleRows
            ,QOverload<int>::of(&QComboBox::highlighted)
@@ -56,51 +55,66 @@ void AlgorithmLattice::placeOperator(QString sign, unsigned int column) {
    // linked list of SignMeasure objects
    if (itemAt(lastInsert,QTransform())){
       QGraphicsItem * p_operatorAtLastInsert= itemAt(lastInsert,QTransform());
-      SignMeasure * p_lastOperator=
+      auto * p_lastOperator=
             qgraphicsitem_cast<SignMeasure *>(p_operatorAtLastInsert);
       p_operatorType->p_CZ= p_lastOperator;
    }
 
    // place the operator
-      // operator, CNOT t upwards arrow
-   if (sign == "CNOT t" % QChar(0x2191)){
+   if (sign == "CNOT t" % QChar(0x2191)){   // operator, CNOT t upwards arrow
       if (*rowMarker > 0){
          unsigned int rowCNOTUpwardsArrow= *rowMarker - 1;
 
-         // (set)Pos = parent coordinates else, scene coordinates
-         p_operatorType->setPos(nodeAddress[rowCNOTUpwardsArrow][column]);
-         columnAtRow[rowCNOTUpwardsArrow] += 1;
+         prepareOperator(*p_operatorType,rowCNOTUpwardsArrow, column);
+         // advance column count at adjacent, up row
          columnAtRow[*rowMarker] += 1;
-
-         lastInsert= nodeAddress[rowCNOTUpwardsArrow][column];
       }
       else
          // CNOT t upwards arrow does not insert a row!!
          return;
    }
-   else {   // all other operators
-      p_operatorType->setPos(nodeAddress[*rowMarker][column]);
-      // retain nodeAddress[...][column] of current row
-      columnAtRow[*rowMarker] += 1;
-
-      lastInsert= nodeAddress[*rowMarker][column];
-   }
-
-   // condition = CNOT t downwards arrow
-   if (sign == "CNOT t" % QChar(0x2193)){
+   else if (sign == "CNOT t" % QChar(0x2193)){   // operator, CNOT t downwards arrow
       unsigned int controlRowColumn= columnAtRow[*rowMarker];
-      prepareRow();   // TO DO: tweak to allow third CNOT of swap proxy
-      columnAtRow[*rowMarker]= controlRowColumn;
+
+      // edge case: three consecutive CNOTs as Swap operator proxy
+      if (p_operatorType->p_CZ->showOperator() == "CNOT t" % QChar(0x2191)){
+         if (controlRowColumn > 2){
+            prepareOperator(*p_operatorType, *rowMarker, column);
+            // align column counts of control and target rows
+            columnAtRow[*maxRowMarker]= controlRowColumn + 1;
+         }
+      }
+      else {
+         prepareOperator(*p_operatorType, *rowMarker, column);
+         addRow();
+         // align column counts of control and target rows
+         columnAtRow[*rowMarker]= controlRowColumn + 1;
+      }
    }
+   else   // non-CNOT operators
+      prepareOperator(*p_operatorType, *rowMarker, column);
 
    addItem(p_operatorType);
-
    p_operators->setModal(true);
 }
 
-void AlgorithmLattice::prepareRow() {
-//   meet requirements common to addRow() and placeOperator() to prepare a
-//   matrix row
+void AlgorithmLattice::prepareOperator(SignMeasure & graphOperator
+                                       , unsigned int row
+                                       , unsigned int column) {
+// meet placement requirements common to CNOT/non-CNOT operators
+// pre-condition: subsidiary to method, placeOperator()
+//   post-condition: N/A
+
+   // (set)Pos = parent coordinates else, scene coordinates
+   graphOperator.setPos(nodeAddress[row][column]);
+   // retain nodeAddress[...][column] of current row
+   columnAtRow[row] += 1;
+
+   lastInsert= nodeAddress[row][column];
+}
+
+void AlgorithmLattice::addRow() {
+//   add a row in response to pre-conditions
 //   pre-condition: either
 //      - user clicks OperatorPalette button, 'Add Row'; OR
 //      - user clicks OperatorPalette button, 'CNOT t downwards arrow',
@@ -123,4 +137,7 @@ void AlgorithmLattice::prepareRow() {
    p_initialiseRow= new SignMeasure(ket0);
    p_initialiseRow->setPos(nodeAddress[*rowMarker][0]);
    addItem(p_initialiseRow);
+
+   // advance column counter at inserted row
+   columnAtRow[*rowMarker] += 1;
 }
