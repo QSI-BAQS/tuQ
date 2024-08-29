5786b2f009bc1ab593ab55344afc2a65c09cd939
Greg <gregory.bowen@student.uts.edu.au>
2024-08-29 18:09:52 +1000

    add datum, latticeColumnsAtRow; remove datum, add_gate and related methods

--- src/gui/mainwindow.hpp
+++ src/gui/mainwindow.hpp
@@ -7,7 +7,6 @@
 #include "mode_simulator/simulatorview.hpp"
 
 #include <QMainWindow>
-#include <QVariant>
 
 
 class MainWindow : public QMainWindow
@@ -18,31 +17,31 @@ class MainWindow : public QMainWindow
    tuQ_mode view_setting {tuQ_mode::modeller};
    tuQ_mode * const p_view_setting= &view_setting;
 
-   GraphSelect * settings;
-   CompilerView * view_compiler;
-   GraphView * view_modeller;
-   SimulatorView * view_simulator;
+   GraphSelect * settings {};
+   CompilerView * view_compiler {};
+   GraphView * view_modeller {};
+   SimulatorView * view_simulator {};
 
-//   QAction * a_addGate;
-   QAction * a_addLattice;
-   QAction * a_compile;
-   QAction * a_openAlgorithm;
-   QAction * a_openGraph;
-   QAction * a_readCircuit;
-   QAction * a_saveAlgorithm;
-   QAction * a_saveGraph;
-   QAction * a_simulate;
+   QAction * a_addLattice {};
+   QAction * a_compile {};
+   QAction * a_openAlgorithm {};
+   QAction * a_openGraph {};
+   QAction * a_readCircuit {};
+   QAction * a_saveAlgorithm {};
+   QAction * a_saveGraph {};
+   QAction * a_simulate {};
 
-   QMenu * circuitMenu;
-   QMenu * editMenu;
-   QMenu * fileMenu;
-   QMenu * graphMenu;
+   QMenu * circuitMenu {};
+   QMenu * editMenu {};
+   QMenu * fileMenu {};
+   QMenu * graphMenu {};
+
+   unsigned int latticeColumnsAtRow [21]= {0};
 
    void addLattice();
    void createMenus();
    void dialogOpen(const QString *);
    void dialogSave(const QString *);
-//   void gatesPalette();
    void readCircuitDialog(const QString *);
    void setActions();
    void setView();
@@ -56,11 +55,11 @@ private slots:
 public:
    explicit MainWindow(QWidget * parent= nullptr);
 
-   const QString * algorithmopenfile;
-   const QString * algorithmsavefile;
-   const QString * graphopenfile;
-   const QString * graphreadcircuit;
-   const QString * graphsavefile;
+   const QString * algorithmopenfile {};
+   const QString * algorithmsavefile {};
+   const QString * graphopenfile {};
+   const QString * graphreadcircuit {};
+   const QString * graphsavefile {};
 };
 
 #endif // TUQ_MAINWINDOW_HPP
