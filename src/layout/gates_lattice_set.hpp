ac0c9426c84959e8ff265fb825fec01950d8a6fc
Greg <gregory.bowen@student.uts.edu.au>
2024-09-11 16:08:31 +1000

    remove redundant data

--- src/layout/gates_lattice_set.hpp
+++ src/layout/gates_lattice_set.hpp
@@ -23,17 +23,6 @@ struct Gate_SE_SW {
 
 // resource cluster references
 static std::vector<Gate_SE_SW> gate_by_address;
-/*
-const char control_cnot[6] {'X','Y','Y','Y','Y','Y'};
-const char target_cnot[6] {'X','X','X','Y','X','X'};
-
-const char h[4] {'X','Y','Y','Y'};   // Hadamard, Clifford
-const char rot[4] {'X','N','N','N'};   // 'arbitrary rotation', non-Clifford
-const char s[4] {'X','X','Y','X'};   // 'pi/2 phase gate', Clifford
-   *** tuQ: see Raussendorf et al. (2003) figures 10, 12 for Swap gate
-const char t[4] {'X','X','N','X'};   // ** PROXY PATTERN ** z-rotation, non-Clifford
-
-*/
 
 // function prototypes
 unsigned long cols_n(std::vector<Gate_SE_SW> &, const nlohmann::json &);
